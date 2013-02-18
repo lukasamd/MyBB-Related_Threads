47a49c8b5807c26e119c5bd44982f5138c86051c
lukasamd <lukasamd@gmail.com>
2013-02-18 12:06:36 +0100

    Add thanks code

--- root/inc/plugins/relatedThreads.php
+++ root/inc/plugins/relatedThreads.php
@@ -128,6 +128,7 @@ class relatedThreads
 
         $plugins->hooks["xmlhttp"][10]["relatedThreads_displayThreads"] = array("function" => create_function('','global $plugins; $plugins->objects[\'relatedThreads\']->displayThreads();')); 
         $plugins->hooks["newthread_start"][10]["relatedThreads_injectNewthread"] = array("function" => create_function('','global $plugins; $plugins->objects[\'relatedThreads\']->injectNewthread();')); 
+        $plugins->hooks["pre_output_page"][10]["relatedThreads_pluginThanks"] = array("function" => create_function('&$arg', 'global $plugins; $plugins->objects[\'relatedThreads\']->pluginThanks($arg);'));
     }
 
     /**
@@ -630,6 +631,23 @@ class relatedThreads
 
         $db->update_query("settings", array("value" => $db->escape_string($val)), "name = 'relatedThreads" . $db->escape_string($name) . "'");
         rebuildsettings();
+    }
+    
+    /**
+     * Say thanks to plugin author - paste link to author website.
+     * Please don't remove this code if you didn't make donate
+     * It's the only way to say thanks without donate :)     
+     */
+    public function pluginThanks(&$content)
+    {
+        global $session, $lukasamd_thanks;
+        
+        if (!isset($lukasamd_thanks) && $session->is_spider)
+        {
+            $thx = '<div style="margin:auto; text-align:center;">This forum uses <a href="http://lukasztkacz.com">Lukasz Tkacz</a> MyBB addons.</div></body>';
+            $content = str_replace('</body>', $thx, $content);
+            $lukasamd_thanks = true;
+        }
     }
 
 }
